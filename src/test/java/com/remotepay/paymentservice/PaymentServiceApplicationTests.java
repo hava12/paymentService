7d69e31a0da83f0b8b476bc818a21484af4680f7
dongyeop <hava12@naver.com>
2021-12-17 22:36:16 +0900

    Initial commit

---
+++ src/test/java/com/remotepay/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remotepay.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
