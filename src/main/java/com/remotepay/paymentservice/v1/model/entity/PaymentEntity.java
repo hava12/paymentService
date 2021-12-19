777427b861d852a048d2409409f051b6479550fd
hava12 <hava12@naver.com>
2021-12-19 22:34:34 +0900

    payment Request 변수 생성 집구하느라 ..

--- src/main/java/com/remotepay/paymentservice/v1/model/entity/PaymentEntity.java
+++ src/main/java/com/remotepay/paymentservice/v1/model/entity/PaymentEntity.java
@@ -2,6 +2,29 @@ package com.remotepay.paymentservice.v1.model.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity(name="payment")
 public class PaymentEntity {
+
+    @Id
+    private Long payment_id;
+
+    @Column
+    private String product_id;
+
+    @Column
+    private String pay_amount;
+
+    @Column
+    private String tax_amount;
+
+    @Column
+    private String service_amount;
+
+    @Column
+    private String phone_number;
 }
